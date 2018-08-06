d671b1f9c3c4b574774bc7f1b96c6c25efe62363
sgerard <sylvain.gerard@docapost.fr>
2018-08-06 14:58:48 +0200

    Lambda 01 & 05

--- Lambda01e/src/dma/FenetreAvecBoutons.java
+++ Lambda01e/src/dma/FenetreAvecBoutons.java
@@ -10,11 +10,14 @@ public class FenetreAvecBoutons extends FenetreBase {
 
 	  public FenetreAvecBoutons() {
 	    super("Lamdas");
-	 
+	    //Lambdae01
 	    button1.addActionListener((ActionEvent event) -> {setBg(Color.BLUE);});
 	    button2.addActionListener(event -> setBg(Color.GREEN));
 	    button3.addActionListener(event -> setBg(Color.RED));
-	    
+	    //Lambdae05
+	    button1.addActionListener(event -> contentPane.setBackground(Color.BLUE));
+	    Color couleurVerte = Color.GREEN;
+	    button2.addActionListener(event -> setBg(couleurVerte));
 	    setVisible(true);
 	  } 
 	}
