a374ff365a473c396e14c21b6f356db35280fd30
sgerard <sylvain.gerard@docapost.fr>
2018-08-06 15:01:41 +0200

    Lambda 04 not finished

--- Lambda04e/src/dma/TriChaines.java
+++ Lambda04e/src/dma/TriChaines.java
@@ -9,6 +9,22 @@ public class TriChaines {
 	System.out.println("tableau d'origine: " + 
     			Arrays.asList(mots));
     
-	// A COMPLETER 
+	Arrays.sort(mots, (s1, s2)->(s1.length()-s2.length()));
+	System.out.println("longueur croissante des chaines: " + 
+			Arrays.asList(mots));
+	
+	Arrays.sort(mots, (s1, s2)->(s2.length()-s1.length()));
+	System.out.println("longueur décroissante des chaines: " + 
+			Arrays.asList(mots));
+	
+	Arrays.sort(mots, (s1, s2)->(s1.charAt(0)-s2.charAt(0)));
+	System.out.println("order alphabétique sur 1e lettre: " + 
+			Arrays.asList(mots));
+	
+	//TODO
+	System.out.println("chaînes contenant la lettre 'e' suivi de celles ne la contenant pas: " + 
+			Arrays.asList(mots));
+	
+	
   }
 }
